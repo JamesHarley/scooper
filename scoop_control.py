42d87bffd9cb0cde36022b671bfb284d1a1bb257
James Harley <jahsnote5@gmail.com>
2018-08-12 19:03:43 -0400

    controller

--- scoop_control.py
+++ scoop_control.py
@@ -1,10 +1,8 @@
 #scoop_control
 #import scoop
-#import scoop_remote
-#import scoop_gigs
+import scoop_remote
+import scoop_gigs
 import scoop_json_gig
 import scoop_json_remote
-
-
 
 print("Finished")
